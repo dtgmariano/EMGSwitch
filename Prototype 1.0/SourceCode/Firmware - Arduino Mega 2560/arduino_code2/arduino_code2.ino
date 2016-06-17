f548b803e36ffead76080b5c3cc0f6d41de6a6d1
felipermartins <felipe.rmartins@outlook.com>
2016-06-17 11:55:50 -0300

    Firmware - Arduino - Atualizando Função SendData()
    Atualizando para enviar vars pela serial.

--- Prototype 1.0/SourceCode/Firmware - Arduino Mega 2560/arduino_code2/arduino_code2.ino
+++ Prototype 1.0/SourceCode/Firmware - Arduino Mega 2560/arduino_code2/arduino_code2.ino
@@ -14,7 +14,9 @@
 #define SAMPFREQ 128                      // ADC sampling rate 256 Hz
 #define TIMER2VAL (1024/(SAMPFREQ))       // Set sampling frequency
 #define RECTIFY 1
-#define CAsize 10 // Size of the vector
+#define CAsize 10 // Size of the Vector
+#define BufferSize 22  //Size of Serial Buffer
+
 #define BAUDRATE 19200
 
 /*Variables*/
@@ -26,7 +28,7 @@ volatile unsigned int uiVecDataChannelA[CAsize];  //Vector for Channel A Data
 unsigned int idxVecA = 0;                         //Index of Vector A
 
 volatile unsigned char TXBuf[VS];          //Transmission packet
-
+bool bOverflow;
 //unsigned int readings[numReadings];       // the readings from the analog input
 //int index = 0;                            // the index of the current reading
 //unsigned total = 0;                       // the running total
@@ -41,14 +43,8 @@ volatile unsigned char TXBuf[VS];          //Transmission packet
 /****************************************************/
 void Toggle_LED1(void)
 {
-  if(digitalRead(LED1)==HIGH)
-  {
-    digitalWrite(LED1,LOW);
-  }
-  else
-  {
-    digitalWrite(LED1,HIGH);
-  }
+    if(digitalRead(LED1)==HIGH) digitalWrite(LED1,LOW);
+    else digitalWrite(LED1,HIGH);
 }
 
 /****************************************************/
@@ -78,43 +74,82 @@ void setup() {
    }*/
 
    Serial.begin(BAUDRATE);
-   FlexiTimer2::set(TIMER2VAL, Timer2_Overflow_ISR);
+   FlexiTimer2::set(TIMER2VAL, DataAquisition_ISR);
    FlexiTimer2::start();
    interrupts();
 }
 
 /****************************************************/
-/*  Function name: Timer2_Overflow_ISR              */
+/*  Function name: loop                             */
+/*  Parameters                                      */
+/*     Input:  No                                   */
+/*    Output:  No                                   */
+/*    Action:  Main                                 */
+/****************************************************/
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    //__asm__ __volatile__ ("sleep");
+    if(idxVecA>=10) SendData();
+}
+
+/****************************************************/
+/*  Function name: DataAquisition_ISR              */
 /*  Parameters                                      */
 /*    Input   :  No                                 */
 /*    Output  :  No                                 */
 /*    Action: Determines ADC sampling frequency.    */
 /****************************************************/
-void Timer2_Overflow_ISR()
+void DataAquisition_ISR()
 {
     Toggle_LED1();
-
     uiADCDataChannelA = analogRead(0);
     //if(RECTIFY == 1) uiDataChannelA = RectifySignal(uiADCDataChannelA);
     //else uiDataChannelA = uiADCDataChannelA;
     uiVecDataChannelA[idxVecA++] = uiDataChannelA;
-
     if(idxVecA>CAsize)
-    //bOverflow = true;
-    //idxVecA++ = 0;
-
+    {
+      bOverflow = true;
+      idxVecA = 0;
+    }
     //TXBuf[0] = ((unsigned char)((ADC_Value & 0b0000001111100000) >> 5) | 0b0000000011100000); //hb 111XXXXX
     //TXBuf[1] = ((unsigned char)((ADC_Value & 0b0000000000011111)));                           //lb 000XXXXX
     //Serial.write(TXBuf[0]);
     //Serial.write(TXBuf[1]);
 }
 
+/****************************************************/
+/*  Function name: RectifySignal                    */
+/*  Parameters                                      */
+/*    Input   :  No                                 */
+/*    Output  :  No                                 */
+/*    Action  :  Send data                     .    */
+/****************************************************/
 unsigned int RectifySignal(unsigned int uiPoint)
 {
     if(uiPoint < aRef) return 2 * aRef - uiPoint;
     else return uiPoint;
 }
 
+/****************************************************/
+/*  Function name: SendData                         */
+/*  Parameters                                      */
+/*    Input   :  No                                 */
+/*    Output  :  No                                 */
+/*    Action  :  Send data                     .    */
+/****************************************************/
+void SendData()
+{
+    int i_Idx_TXBuf = 0;
+    TXBuf[0] = 0b0000001100110011   //Header
+    for(int i=0; i<idxVecA; i+=2)
+    {
+        TXBuf[(i_Idx_TXBuf++)+1] = ((unsigned char)((uiVecDataChannelA[i] & 0b0000001111100000) >> 5) | 0b0000000011100000); //hb 111XXXXX
+        TXBuf[(i_Idx_TXBuf++)+1] = ((unsigned char)((uiVecDataChannelA[i] & 0b0000000000011111)));                           //lb 000XXXXX
+    }
+    TXBuf[i_Idx_TXBuf+1] = 0b0000110011001100 //End
+    idxVecA = 0;
+}
 
 void filt()
 {
@@ -146,9 +181,4 @@ void filt()
   //    TXBuf[1] = ((unsigned char)((average & 0b0000000000011111)));                           //lb 000XXXXX
   //    Serial.write(TXBuf[0]);
   //    Serial.write(TXBuf[1]);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  __asm__ __volatile__ ("sleep");
 }
